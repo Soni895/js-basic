163b427ba059b168d01c4f1f6c0bb2c73fab9d02
soni895 <darshansoni895@gmail.com>
2023-05-18 16:55:29 +0530

    object created

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 // console.log("hi i am darshan kumar soni");
 
-console.log("darshan soni");
+// console.log("darshan soni");
 
- const  b=50;
- a=5;
+//  const  b=50;
+//  a=5;
 
 
-if (a < 0 || b<0) {
- let result = 'positive';
- console.log(result,"\n",a);
-//  console.log(a);
-} else {
-  let result = 'NOT positive';
-  console.log(result);
-}
+// if (a < 0 || b<0) {
+//  let result = 'positive';
+//  console.log(result,"\n",a);
+// //  console.log(a);
+// } else {
+//   let result = 'NOT positive';
+//   console.log(result);
+// }
 
-for(let i=0;i<20;i++)
+// for(let i=0;i<20;i++)
 
-{
-    console.log(i,"\nhi darshan soni \n");
-}
+// {
+//     console.log(i,"\nhi darshan soni \n");
+// }
  let arr=['darsahn','soni',25,30,50];
  let i=0;
-while(i<arr.length)
+ 
+// while(i<arr.length)
+// {
+//     console.log(arr[i]);
+//     i++;
+// }
+// arr.push("hi i am darshan soni");
+// i=0;
+// while(i<arr.length)
+// {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+console.log(arr.length);
+arr.push("hi i am darshan soni");
+console.log(arr.length);
+// boject creation
+let student=
+
 {
-    console.log(arr[i]);
-    i++;
-}
+    rollno:11212530,
+    age:38,
+    subject:"Btech",
+};
+// console.log(student["rollno"]);
+

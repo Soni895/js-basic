d7dbaf3efdf5c105df7d327f46443115a6c4778a
soni895 <darshansoni895@gmail.com>
2023-05-18 16:11:18 +0530

    basic_1 comleted

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi i am darshan kumar soni");
+// console.log("hi i am darshan kumar soni");
+
+console.log("darshan soni");
+
+ const  b=50;
+ a=5;
+
+
+if (a < 0 || b<0) {
+ let result = 'positive';
+ console.log(result,"\n",a);
+//  console.log(a);
+} else {
+  let result = 'NOT positive';
+  console.log(result);
+}
+
+for(let i=0;i<20;i++)
+
+{
+    console.log(i,"\nhi darshan soni \n");
+}

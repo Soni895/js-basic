e32624f60d152f7673e447d1e09f977d72f2dd88
soni895 <darshansoni895@gmail.com>
2023-05-21 00:44:12 +0530

    rest_operator

--- index.js
+++ index.js
@@ -425,6 +425,23 @@ function get_sum()
 console.log(get_sum(5,8,9,9,9,9,20));
 
 
+// rest operator
+
+function get_sum_rest_operator( ...etc)
+{
+    let sum=0;
+    console.log("rest priented",etc);
+    for(let i of etc)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+console.log(get_sum_rest_operator(12,23));
+
+
+
+
 
 
 

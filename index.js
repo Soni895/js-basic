a8df488d7a2f0b284b4d44824d5b295d8e10ec5f
soni895 <darshansoni895@gmail.com>
2023-05-20 17:47:09 +0530

    for of loop

--- index.js
+++ index.js
@@ -328,12 +328,19 @@ console.log(temp2);
 
 
 // spread operator in js
-let arr1=[1,2,3,4,5];
-let arr2=[6,7,8,9,10];
-let ans_arr=[...arr1,'darshan','soni',true,...arr2];
-console.log(ans_arr);
-let copy_arr1=[...arr1];
-arr1.splice(1,1,"darshan");
-console.log(copy_arr1)
+// let arr1=[1,2,3,4,5];
+// let arr2=[6,7,8,9,10];
+// let ans_arr=[...arr1,'darshan','soni',true,...arr2];
+// console.log(ans_arr);
+// let copy_arr1=[...arr1];
+// arr1.splice(1,1,"darshan");
+// console.log(copy_arr1);
 
 // for of loop  which is used for iteration
+let arr1=[1,2,3,4,5];
+
+for(let i of arr1)
+{
+    console.log(i);
+}
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,7 @@
-// let arr1=[1,2,3,4,5];
+let arr1=[1,2,3,4,5];
 // let arr2=[6,7,8,9,10];
+// let ans_arr=[...arr1,'darshan','soni',true,...arr2];
+// console.log(ans_arr);
+// let copy_arr1=[...arr1];
+// arr1.splice(1,1,"darshan");
+// console.log(copy_arr1);

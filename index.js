cb65889b6f2548031d9a63f409f875f39656f3a8
soni895 <darshansoni895@gmail.com>
2023-05-20 13:44:57 +0530

    insertion in arr

--- index.js
+++ index.js
@@ -197,6 +197,21 @@ let time=date.getTime();
 console.log(time);
 
 // array start
+// craetion
+let arr=[1,2,3,4,5];
+console.log(arr);
+console.log(arr[3]);
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[1]);
+}
 
+// insertion
+arr.push(9);
+arr.unshift(0);
+console.log(arr);
+
+arr.splice(2,0,"darsahn","soni");
+console.log(arr);
 
 

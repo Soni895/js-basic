9abc41c0233bb70bda0293c9329fdf94788fa579
soni895 <darshansoni895@gmail.com>
2023-05-21 18:00:23 +0530

    dom added

---
+++ dom_manipulation-modernjs.js
@@ -0,0 +1,12 @@
+console.log("hi darshan soni");
+
+const x=[1,2,3,4,5];
+x.push("darshan");
+console.log(x.length);
+console.log(x);
+const a=35;
+let b=30;
+console.log(` the sum of aand{b}is${a+b}`);
+console.log();
+let slice=x.slice(2);
+console.log(slice);
